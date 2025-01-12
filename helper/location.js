f2105fbdd7794c28b2f55914bc83bfdaeea1b141
dinesh subhash pawar <dspfamily8@gmail.com>
2025-01-12 23:40:40 +0530

    changes

--- helper/location.js
+++ helper/location.js
@@ -4,6 +4,7 @@ export async function getLocationName(lat, lon) {
   const apiKey = "YOUR_GOOGLE_MAPS_API_KEY"; // Replace with your API key
   const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`;
 
+  
   try {
     const response = await axios.get(url);
     if (true) {
